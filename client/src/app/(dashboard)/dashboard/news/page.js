929d8b73edd6a2e8528034129adece20a0b5a6ab
Aryaman <109002742+Aryamann27@users.noreply.github.com>
2023-10-25 22:08:38 +0530

    news api implement

--- client/src/app/(dashboard)/dashboard/news/page.js
+++ client/src/app/(dashboard)/dashboard/news/page.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 const NewsPage = () => {
     const [techNews, setTechNews] = useState([]);
     const [scienceNews, setScienceNews] = useState([]);
+    const [businessNews, setBusinessNews] = useState([])
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -27,6 +28,16 @@ const NewsPage = () => {
           .catch((error) => console.error('Error fetching data:', error));
     }, []);
 
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=43551de3a3274f8196dca4b37d12e012') 
+          .then((response) => response.json())
+          .then((data) => {
+            setBusinessNews(data.articles);
+            setLoading(false);
+          })
+          .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
     return <div>
 
       <div style={{display:"flex"}}>
@@ -34,8 +45,8 @@ const NewsPage = () => {
           <h1 style={{fontSize:"2rem"}}>Technology</h1>
           <hr />
           <br />
-        {techNews.map((article, index) => ( 
-        <div style={{marginBottom:"1rem"}} key={index} className="col-span-8 md:col-span-2 h-48 bg-gray-800 rounded-lg border border-gray-700 px-3 py-3">
+        {techNews.slice(0, 5).map((article, index) => ( 
+        <div style={{marginBottom:"1rem"}} key={index} className="col-span-8 md:col-span-2 h-50 bg-gray-800 rounded-lg border border-gray-700 px-3 py-3">
             <h1 style={{fontSize:"1.5rem", fontWeight:"bolder"}}>{article.title}</h1>
             <p>{article.description}</p>
             <a href={article.url} target="_blank" rel="noopener noreferrer">
@@ -48,8 +59,8 @@ const NewsPage = () => {
           <h1 style={{fontSize:"2rem"}}>Science</h1>
           <hr />
           <br />
-        {scienceNews.map((article, index) => ( 
-        <div style={{marginBottom:"1rem"}} key={index} className="col-span-8 md:col-span-2 h-48 bg-gray-800 rounded-lg border border-gray-700 px-3 py-3">
+        {scienceNews.slice(0, 5).map((article, index) => ( 
+        <div style={{marginBottom:"1rem"}} key={index} className="col-span-6 md:col-span-2 h-50 bg-gray-800 rounded-lg border border-gray-700 px-3 py-3">
             <h1 style={{fontSize:"1.5rem", fontWeight:"bolder"}}>{article.title}</h1>
             <p>{article.description}</p>
             <a href={article.url} target="_blank" rel="noopener noreferrer">
@@ -57,6 +68,21 @@ const NewsPage = () => {
             </a>
         </div> ))}
         </div>
+        
+        <div style={{marginLeft:"1rem"}}>
+          <h1 style={{fontSize:"2rem"}}>Business</h1>
+          <hr />
+          <br />
+        {businessNews.slice(0, 5).map((article, index) => ( 
+        <div style={{marginBottom:"1rem"}} key={index} className="col-span-6 md:col-span-2 h-50 bg-gray-800 rounded-lg border border-gray-700 px-3 py-3">
+            <h1 style={{fontSize:"1.5rem", fontWeight:"bolder"}}>{article.title}</h1>
+            <p>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+              Read more
+            </a>
+        </div> ))}
+        </div>
+
         </div>
     </div>;
 };
